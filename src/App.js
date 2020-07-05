1deb979a97818c8b3155863e67b3620bdce7eb49
arihant-2310 <arihant2310@gmail.com>
2020-07-05 23:19:46 +0530

    adds auto color change for weather card based on hot temperature

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import WeatherCard from "./components/WeatherCard/component";
 function App() {
   return (
     <div className="App">
-      <WeatherCard />
+      <WeatherCard temp="20" />
+      <WeatherCard temp="40" />
+      <WeatherCard temp="12" />
     </div>
   );
 }
--- src/components/WeatherCard/component.js
+++ src/components/WeatherCard/component.js
@@ -4,10 +4,16 @@ import Location from "./Location";
 import Icon from "./Icon";
 import Condition from "./Condition";
 
-const WeatherCard = () => {
+const WeatherCard = (props) => {
+  let highColor = (1 - (props.temp - 12) / 28) * 255;
+  let lowColor = highColor - 150;
   const Card = styled.div`
     margin: 10px auto;
-    background: linear-gradient(to top, blue, lightblue);
+    background: linear-gradient(
+      to top,
+      rgb(255, ${highColor}, 0),
+      rgb(255, ${lowColor}, 0)
+    );
     width: 200px;
     height: 240px;
     display: flex;
